ff384886378d78a1eaaceddbed7adbcf86530d87
JAntonioP <ptc12495@gmail.com>
2017-01-27 13:27:24 -0600

    Add files via upload

---
+++ Particulas avance en casa.py
@@ -0,0 +1,11 @@
+Permitidos = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6]
+from random import choice
+N = []
+for particulas in range(8):
+	coord = [choice(Permitidos) for x in range(3)]
+	N.append(coord)
+print (N)
+from math import sqrt
+for particulas in N:
+	dist= sqrt(sum([x**2 for x in particulas]))
+	print (dist)
